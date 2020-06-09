23c88c30c460612e5e10804c75478a3310500ce2
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-09 16:58:56 -0400

    Made the fourth exercise of challenge #1

--- DOM/desafio-01/app.js
+++ DOM/desafio-01/app.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#desafio',
     data: {
         nome: 'Kelly Sandim',
-        idade: 25        
+        idade: 25,
+        linkImagem: 'https://criticalhits.com.br/wp-content/uploads/2020/01/sarada-chidori-boro-boruto-manga-42.jpg'        
     },
     methods: {
         randomNumber: function() {            
