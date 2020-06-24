083313437d57e5369e09eecfe9a44f64653eded5
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-24 16:52:13 -0400

    Made the first exercise of challenge #3

--- DOM/desafio-03/app.js
+++ DOM/desafio-03/app.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#desafio',
     data: {
         valor: 0
-    }
+    },
+    computed: {
+        resultado () {                
+            return this.valor === 37 ? "Valor Igual" : "Valor Diferente";
+        }
+    },
 });
