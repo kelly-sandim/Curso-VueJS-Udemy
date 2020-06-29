1ab77347571fcdbe0001e61879bff24e608c0d2a
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-29 16:58:34 -0400

    Project #1 - Seventh Part

--- Projeto-01-Monstro/app.js
+++ Projeto-01-Monstro/app.js
@@ -18,13 +18,21 @@ new Vue({
         },
         attack(special) {
             this.hurt('monsterLife', 5, 10, special);
-            this.hurt('playerLife', 7, 12, false);
+            this.hurt('playerLife', 7, 12, false); //player não pode ser ferido com golpe especial
         },
         hurt(prop, min, max, special) {
             const plus = special ? 5 : 0;
             const hurt = this.getRandom(min + plus, max + plus);
             this[prop] = Math.max(this[prop] - hurt, 0); //o 0 evita que o PlayerLife seja negativo
 
+        },
+        healAndHurt() {
+            this.heal(10, 15);
+            this.hurt('playerLife', 7, 12, false);
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max);
+            this.playerLife = Math.min(this.playerLife + heal, 100); //assim como o de cima, esse 100 impede que a vida do jogador vá mais do que isso
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min;
