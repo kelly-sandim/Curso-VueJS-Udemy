4e7aaf5a19aff7f03049f7a9b47e62b17626d5b5
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-07-06 10:02:34 -0400

    Components: Example 3

--- componentes/componente-intro/src/main.js
+++ componentes/componente-intro/src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Contador from './Contador.vue'
+import Contadores from './Contadores.vue'
 
 Vue.config.productionTip = false
-//agora de forma global vc pode usar esse componente Contador
-Vue.component('app-contador', Contador)
+//agora de forma global vc pode usar esse componente Contadores
+//Forma Global
+Vue.component('app-contadores', Contadores)
 
 new Vue({
   render: h => h(App),
