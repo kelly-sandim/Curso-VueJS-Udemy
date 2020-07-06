9d02d08177f63246d13cfe5726e2ff3f72b13674
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-07-06 10:17:16 -0400

    Components: Example 4

--- componentes/componente-intro/src/Contador.vue
+++ componentes/componente-intro/src/Contador.vue
@@ -2,7 +2,7 @@
     <div class="contador">
         <span>{{ contador }}</span>
         <button @click="adicionar">+</button>
-        <button @click="subtrair">-</button>
+        <button @click="subtrair">-</button>        
     </div>  
 </template>
 
@@ -24,14 +24,14 @@ export default {
 }
 </script>
 
-<style>
-    .contador span {
+<style scoped>   
+    span {
         border-bottom: 1px solid #ccc;
         height: 30px;
         padding: 5px 25px;
     }
 
-    .contador button {
+    button {
         height: 30px;
         width: 30px;
         border-radius: 15px;
