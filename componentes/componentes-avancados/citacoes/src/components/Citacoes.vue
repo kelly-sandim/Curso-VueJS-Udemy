cc876669764c8fc563220c6bbf797948e9d053a9
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-07-13 08:54:55 -0400

    Components: Example 19

--- componentes/componentes-avancados/citacoes/src/components/Citacoes.vue
+++ componentes/componentes-avancados/citacoes/src/components/Citacoes.vue
@@ -5,8 +5,8 @@
             <button @click="numero++">&gt;</button>
         </span>
         <Citacao>
-            <h1 slot="autor">{{ citacoes[indice].autor }}</h1>
-            <p slot="texto">{{ citacoes[indice].texto }}</p>
+            <h1 slot="autor">{{ citacoes[indice].autor }}</h1>                        
+            <p>{{ citacoes[indice].texto }}</p>
             <h6 slot="fonte">{{ citacoes[indice].fonte }}</h6>
         </Citacao>
     </div>
