f590b43cf09ad9960cf72bfce72b6e48b7c2fa4d
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-07-13 08:58:15 -0400

    Components: Slot summary

--- componentes/componentes-avancados/citacoes/src/components/Citacao.vue
+++ componentes/componentes-avancados/citacoes/src/components/Citacao.vue
@@ -2,7 +2,7 @@
     <div class="citacao">
         <slot name="fonte"></slot>
         <slot></slot>
-        <slot name="autor"></slot>
+        <div class="autor"><slot name="autor"></slot></div>
     </div>
 </template>
 
@@ -18,5 +18,9 @@ export default {
         background-color: rgba(0, 0, 0, .1);
         padding: 25px;
         max-width: 450px;
-    }   
+    }  
+
+    .autor {
+        float: right;
+    } 
 </style>
