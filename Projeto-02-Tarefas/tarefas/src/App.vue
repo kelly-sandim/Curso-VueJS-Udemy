553958b5fe7fa3bca5c2d642322c864b20a68a39
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-07-14 14:47:43 -0400

    Project #2 - Eighth Part

--- Projeto-02-Tarefas/tarefas/src/App.vue
+++ Projeto-02-Tarefas/tarefas/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TasksProgress :progress="progress" />
 		<NewTask @taskAdded="addTask" />
 		<TaskGrid
 				:tasks="tasks" 
@@ -10,14 +11,22 @@
 </template>
 
 <script>
+import TasksProgress from './components/TasksProgress.vue'
 import NewTask from './components/NewTask.vue'
 import TaskGrid from './components/TaskGrid.vue'
 
 export default {
-	components: { NewTask, TaskGrid },
+	components: { TasksProgress, NewTask, TaskGrid },
 	data() {
 		return {
 			tasks: []
+		}
+	},
+	computed: {
+		progress() {
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
 		}
 	},
 	methods: {
